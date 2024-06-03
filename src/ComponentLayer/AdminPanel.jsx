627831af86fe2f98d0ff8c36c1670ad6b25cab52
Johnsey <sruthesis@gmail.com>
2024-06-03 21:29:03 +0700

    add Qr Entry

--- src/ComponentLayer/AdminPanel.jsx
+++ src/ComponentLayer/AdminPanel.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import Sidebar from '../layout/SideBar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Dashbaord from './AdminPanel/Dashbaord';
 import QRStudentEntry from './QrEntry/QRStudentEntry';
 import { HideSideBarProvidor } from '../Context/HideSidebarContext';
-import SideBarToggle from '../layout/Component/SideBarToggle'
+import SideBarToggle from '../layout/Component/SideBarToggle';
 import { ThemeSwitchProvider } from '../Context/ThemeSwitchContext';
+
 function AdminPanel() {
+    const pathLocation = useLocation();
 
     return (
-        //This cotext ise for show and hide sidebar
         <HideSideBarProvidor>
             <ThemeSwitchProvider>
                 <main className="flex h-screen sm:h-screen relative">
-                    <div className="sideBarToggle absolute p-10 w-full flex flex-col sm:hidden items-end z-50">
-                        {/* for hide or show sidebar when in mobile view  */}
-                        <SideBarToggle />
-                    </div>
-                    <Sidebar /> {/* Set a fixed width for the sidebar */}
-                    <section className="flex-1 p-5 xl:h-full overflow-y-auto sm:overscroll-y-none"> {/* This will make the content area take up the remaining space */}
+                    {/* Check if the current path is not "/QRStudentEntry" */}
+                    {pathLocation.pathname !== "/QRStudentEntry" && (
+                        <>
+                            <div className="sideBarToggle absolute p-10 w-full flex flex-col sm:hidden items-end z-50">
+                                {/* for hide or show sidebar when in mobile view  */}
+                                <SideBarToggle />
+                            </div>
+                            <Sidebar />
+                        </>
+                    )}
+
+                    {/* Always render the Sidebar component */}
+
+
+                    <section className="flex-1 p-5 xl:h-full overflow-y-auto sm:overscroll-y-none">
+                        {/* This will make the content area take up the remaining space */}
                         <Routes>
                             <Route path="/" element={<Dashbaord />} />
                             <Route path="/QRStudentEntry" element={<QRStudentEntry />} />
--- src/ComponentLayer/QrEntry/QRStudentEntry.jsx
+++ src/ComponentLayer/QrEntry/QRStudentEntry.jsx
@@ -1,9 +1,43 @@
+import { Scanner } from '@yudiel/react-qr-scanner'
 import React from 'react'
 
 function QRStudentEntry() {
+    
+    const handleScanResult = (text) => {
+        if (text) {
+            try {
+                // Attempt to create a URL object from the scanned text
+                const url = new URL(text);
+
+                // Check if the URL contains the 'name' parameter
+                const getStudentID = url.searchParams.get('name');
+                if (getStudentID) {
+                    // If 'name' parameter exists, disable scanning and set the student ID
+                    setEnabledScan(false);
+                    setStudentID(getStudentID);
+                }
+            } catch (error) {
+                // Scanned text is not a valid URL
+                alert("Student Not Exit OR Wrong QR Code Type");
+            }
+        }
+    };
     return (
-        <div className='w-full h-full bg-blue-300'>
-            QRStudentEntry
+        <div className='w-full h-full'>
+            <div className="ScanQR-ConfirmForm w-[390px] h-full rounded-[20px]">
+                <div className="flex flex-col CamScanQR bg-secondary w-f h-fit rounded-[20px] p-5">
+                    <div className="headerCamScanQR text-accent h-[46px]">
+                        <p>Scan Your QR Here</p>
+                    </div>
+                    <div className="w-[350px] h-[350px]">
+                        <Scanner
+                            onResult={(text) => handleScanResult(text)}
+                            // enabled={enabledScan}
+                            components={{ tracker: true, audio: false }}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
