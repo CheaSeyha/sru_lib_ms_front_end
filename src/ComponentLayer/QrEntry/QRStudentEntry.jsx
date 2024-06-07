5401701aa1abfcb98d8f7129678b007d4ec0b0b3
Johnsey <sruthesis@gmail.com>
2024-06-07 22:30:55 +0700

    Fix Mobile Responsive Scrren

--- src/ComponentLayer/QrEntry/QRStudentEntry.jsx
+++ src/ComponentLayer/QrEntry/QRStudentEntry.jsx
@@ -9,15 +9,15 @@ function QRStudentEntry() {
 
     return (
         <ScanResultIDProvider>
-            <div className='w-full h-fit sm:h-full flex flex-col-reverse sm:flex-row space-x-5 overflow-auto'>
-                <div className="flex flex-col space-y-5 ScanQR-ConfirmForm w-[150px] sm:w-[230px] lg:w-[290px] xl:w-[390px] rounded-[20px]">
+            <div className='w-full h-fit sm:h-full flex flex-col-reverse sm:flex-row space-x-0 sm:space-x-5 overflow-auto'>
+                <div className="flex flex-col space-y-5 ScanQR-ConfirmForm w-full sm:w-[230px] lg:w-[290px] xl:w-[390px] rounded-[20px]">
                     {/* <!-- Show Scan Camera  --> */}
                     <CameraScanQR />
                     {/* <!-- Form Show Student info after scan  --> */}
                     <FormConfirmEntry />
                 </div>
-                <div className="flex-1 sm:flex flex-col data-of-entry-table w-full h-full sm:space-y-5 overflow-auto">
-                    <div className="student-entry-card w-full h-[104px] grid grid-cols-3 sm:gap-5">
+                <div className="flex-1 sm:flex flex-col data-of-entry-table w-full h-full sm:space-y-5 overflow-auto mb-5 sm:mb-0">
+                    <div className="student-entry-card w-full h-[104px] grid grid-cols-3 gap-5">
                         <CardDataOfStudentEntry amountData={17} bgIcon="bg-[#00FF29]" cardType="Entry" cardTypeDetail="Total Entry Today" iconCard={<DoorOpen className='w-full h-full text-white' />} bgColor="from-[#00C31F] to-[#1F9EB2]" />
                         <CardDataOfStudentEntry amountData={2} bgIcon="bg-[#00D1FF]" cardType="Exit" cardTypeDetail="Total Exit Today" iconCard={<LogOut className='w-full h-full text-white' />} bgColor="from-[#00A4C8] to-[#C0C0C0]" />
                         <CardDataOfStudentEntry amountData={19} bgIcon="bg-[#C5F3B3]" cardType="Total" cardTypeDetail="Total Student Entry" iconCard={<UsersRound className='w-full h-full text-white' />} bgColor="from-[#C0C0C0] to-[#50FF00]" />
