c26977663d5ce1913ac46bb944f0de45fc3d2e62
Johnsey <sruthesis@gmail.com>
2024-08-22 21:17:36 +0700

    Fix Chart Not Change Color Light Mode In Form Analtic

--- src/ComponentLayer/Analytic/AnalyticForm.jsx
+++ src/ComponentLayer/Analytic/AnalyticForm.jsx
@@ -18,27 +18,29 @@ function AnalyticForm() {
     return (
         <main className='flex flex-col w-full'>
             <div className="headr-card w-full h-full space-y-5">
-                <div className="bg-secondary rounded-[20px] p-5 space-y-5 text-accent">
+                <div className="text-accent">
                     <div className="book-form font-noto w-full flex justify-between relative items-center">
-                        <p>ទិន្ន័យសៀវភៅ</p>
+                        <div className='h-[46px] px-5 flex rounded-[10px] items-center bg-secondary'>
+                            <p>ទិន្ន័យសៀវភៅ</p>
+                        </div>
                         <button className="btn bg-secondary block lg:hidden text-accent me-7 sm:me-0" onClick={handleHideDropDownButton}>
                             <SquareMenu />
                         </button>
-                        <div className={`container-button flex ${hideDropDownButton ? "hidden" : "block"} z-20 top-[70px] lg:top-0 lg:flex gap-[5px] absolute flex-col lg:flex-row lg:relative right-6 sm:right-0 rounded-lg drop-shadow-md lg:drop-shadow-none p-2 lg:p-0 bg-secondary lg:bg-transparent`}>
+                        <div className={`container-button flex ${hideDropDownButton ? "hidden" : "block"} z-20 top-[70px] lg:top-0 lg:flex gap-5 absolute flex-col lg:flex-row lg:relative right-6 sm:right-0 rounded-lg drop-shadow-md lg:drop-shadow-none p-2 lg:p-0 bg-secondary lg:bg-transparent`}>
                             <div className="date-picker">
                                 <Datepicker
                                     showShortcuts={true}
                                     showFooter={true}
                                     value={value}
                                     onChange={newValue => setValue(newValue)}
-                                    inputClassName="bg-primary h-[48px] px-5 rounded-[10px] w-[290px] text-accent"
+                                    inputClassName="bg-secondary h-[48px] px-5 rounded-[10px] w-[290px] text-accent"
                                 />
                             </div>
-                            <button className="btn btn-primary font-noto">
+                            <button className="btn btn-secondary font-noto">
                                 <FileDown />
                                 <p>ទាញរបាយការណ៍ប្រចាំខែ</p>
                             </button>
-                            <button className="btn btn-primary font-noto">
+                            <button className="btn btn-secondary font-noto">
                                 <FileBarChartIcon />
                                 បង្កើតរបាយករណ៏
                             </button>
